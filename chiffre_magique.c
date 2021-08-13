16e2b83534227452c9774a9a6bd9aa7f59e7da3c
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2021-08-13 14:24:43 +0200

    Plus simple

--- chiffre_magique.c
+++ chiffre_magique.c
@@ -11,24 +11,23 @@ int main(void)
 
     // printf("Le chiffre aléatoire est %d.\n", randnum);
 
-    while (answer != randnum && trials != 0)
+    while (trials != 0)
     {
         printf("Trouvez le chiffre magique entre 1 et 10 !\n");
         printf("%d essai(s) restant.\n", trials);
         printf("Saisissez un chiffre entre 1 et 10 : ");
         scanf("%d", &answer);
-        if (answer != randnum)
+        
+        if (answer == randnum)
         {
-            printf("Vous n'avez pas trouvé le chiffre magique.\n");
-            --trials;
+            printf("Bravo ! Vous avez trouvé le chiffre magique : %d.", randnum);
+            
+            return 0;
         }
+        
+        printf("Vous n'avez pas trouvé le chiffre magique.\n");
+        --trials;
     }
-    if (answer == randnum)
-    {
-        printf("Bravo ! Vous avez trouvé le chiffre magique : %d.", randnum);
-    }
-    else
-    {
-        printf("Dommage ! Vous n'avez plus d'essais.");
-    }
+
+    printf("Dommage ! Vous n'avez plus d'essais.");
 }
